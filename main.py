627b36d1d406c37d8d12ea14f8ff246cb965dcc6
VihaanP2710 <v.4956194@gmail.com>
2025-03-09 14:55:48 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Print a sentance
+print("Welcome to the world of programming!")
+
+# Print a number
+print(4)
+
+# Next line after print
+print("Hello Python \n")
+
+# Pass multiple arguments for printing
+print("hello", 5)
+
+# End argument of print statement
+print("welcome to ", end="*")
