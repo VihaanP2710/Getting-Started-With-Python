52596ee9a3935ddd323107b6ae95b1a75d946308
VihaanP2710 <v.4956194@gmail.com>
2025-03-09 15:00:55 +0530

    added variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Creating different Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User input
+name = input("Enter your name: ")
+
+print("\nHello, ", name, "\nWelcome to Codingal!")
